4a80a3b740a7286df18c1a69d15cae0904ce1b89
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-06 13:23:57 +0200

    Button setup

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Button button = findViewById(R.id.activityButton);
+        button.setOnClickListener(view -> {
+            
+        });
         setSupportActionBar(toolbar);
     }
 
