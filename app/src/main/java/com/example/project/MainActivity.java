3ed87a04e2c2b12a11b35be48d8218e4dd3be58d
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-06 14:06:37 +0200

    Sets preference from input on button click

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,24 +3,32 @@ package com.example.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
         Button button = findViewById(R.id.activityButton);
+
         button.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, SharedActivity.class));
         });
+
         setSupportActionBar(toolbar);
+    }
+
+    public void displaySharedText() {
+        
     }
 
 }
--- app/src/main/java/com/example/project/SharedActivity.java
+++ app/src/main/java/com/example/project/SharedActivity.java
@@ -2,13 +2,33 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SharedActivity extends AppCompatActivity {
+
+    private EditText edit;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared);
+        edit = findViewById(R.id.edit);
+        button = findViewById(R.id.buttonSave);
+        button.setOnClickListener(view -> {
+            save(edit.getText().toString());
+            edit.setText("");
+        });
+    }
+
+    public void save(String data) {
+        SharedPreferences pref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("input", data);
+        editor.apply();
     }
 }
